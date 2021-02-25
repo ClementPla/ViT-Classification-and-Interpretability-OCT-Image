6b557a70dd5078d4de074bc09ba8799a2230fb29
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2021-02-25 12:09:45 -0500

    Fix some bugs

--- networks/networks.py
+++ networks/networks.py
@@ -56,6 +56,8 @@ def get_network(config, img_size=None):
             del state_dict['head' + '.bias']
             del state_dict['head_dist' + '.weight']
             del state_dict['head_dist' + '.bias']
+            if img_size != 384:
+                del state_dict['pos_embed:']
             network.load_state_dict(state_dict, strict=False)
 
     if config['architecture'] == 'vit_large_patch16_384':
@@ -70,6 +72,8 @@ def get_network(config, img_size=None):
             state_dict = load_state_dict_from_url(url_pretrained, map_location='cpu')['state_dict_ema']
             del state_dict['head' + '.weight']
             del state_dict['head' + '.bias']
+            if img_size != 224:
+                del state_dict['pos_embed:']
             network.load_state_dict(state_dict, strict=False)
     if config['architecture'] == 'T2T-ViT_t-24':
         from .T2T.models import T2t_vit_t_24
@@ -79,6 +83,8 @@ def get_network(config, img_size=None):
             state_dict = load_state_dict_from_url(url_pretrained, map_location='cpu')['state_dict_ema']
             del state_dict['head' + '.weight']
             del state_dict['head' + '.bias']
+            if img_size != 224:
+                del state_dict['pos_embed:']
             network.load_state_dict(state_dict, strict=False)
 
     if config['architecture'] == 'T2T-ViT-19':
@@ -89,6 +95,8 @@ def get_network(config, img_size=None):
             state_dict = load_state_dict_from_url(url_pretrained, map_location='cpu')['state_dict_ema']
             del state_dict['head' + '.weight']
             del state_dict['head' + '.bias']
+            if img_size != 224:
+                del state_dict['pos_embed:']
             network.load_state_dict(state_dict, strict=False)
 
     if config['architecture'] == 'OpticNet':
