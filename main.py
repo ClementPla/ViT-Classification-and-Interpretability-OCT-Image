70d189f4c3f55f3e739ce7cc4f818dce62c78989
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2021-02-25 11:33:16 -0500

    Add T2T models with downloadable models

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from experiment import OCTClassification
 if __name__ == '__main__':
     config_path = 'configs/config.yaml'
     config = Config(config_path)
-    config['Manager']['run'] = 'vit_deit_base_distilled_patch16_384'
+    config['Manager']['run'] = 'T2T-ViT-14'
     config['Network']['architecture'] = config['Manager']['run']
 
     experiment = OCTClassification(config)
