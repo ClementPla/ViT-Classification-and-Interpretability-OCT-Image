8ff53544830cd00b2b0d6d97c03572961556ea0e
Clément Playout <clpla@liv4dgpu.rdgi.polymtl.ca>
2021-02-25 10:49:22 -0500

    Commit main file for local changes

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ if __name__ == '__main__':
     config['Manager']['run'] = 'vit_deit_base_distilled_patch16_384'
     config['Network']['architecture'] = config['Manager']['run']
 
-    experiment = OCTClassification(config)
+    experiment = OCTClassification(config, '5c44af96831947abb24baaaa4c1efd60')
+    experiment.run_training = False
     experiment.start()
