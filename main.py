f7e0ce69cf5b6dfa5fafc10ada75613142004130
ClementPla <clement.playout@polymtl.ca>
2021-03-25 12:31:25 -0400

    Fix fundus config file to new format

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from nntools.utils import Config
 from experiment import OCTClassification
+from fundus_experiment import FundusClassification
 
 if __name__ == '__main__':
-    config_path = 'configs/config.yaml'
+    config_path = 'configs/config_fundus.yaml'
     config = Config(config_path)
     config['Manager']['run'] = 'ResNet152'
     config['Network']['architecture'] = config['Manager']['run']
 
-    experiment = OCTClassification(config)
+    experiment = FundusClassification(config)
     experiment.start()
